07188811a00300518f15de3de31c988342b7176c
mabidoo-bit <mabidoo@hotmail.fr>
2026-09-13 23:50:49 +0200

    Fix icon paths in service worker
    Updated icon file paths in the service worker.

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ const FILES_TO_CACHE = [
   "./products.js",
   "./payment.js",
   "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
+  "./icon-192.png",
+  "./icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
